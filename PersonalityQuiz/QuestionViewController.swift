d97312b88365f2c299ec240d8c7203667115d6d1
Salvador Garcia <salvadorgarcia@salvadors-macbook-pro.local>
2019-03-04 22:32:13 -0600

    Added update functions for the single, multiple, and ranged stack vies. Modified the updateUI function to display the current question and progress.

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -11,9 +11,27 @@ import UIKit
 class QuestionViewController: UIViewController {
 
    
+    @IBOutlet weak var questionLabel: UILabel!
+    
     @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
     @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multipleLabel1: UILabel!
+    @IBOutlet weak var multipleLabel2: UILabel!
+    @IBOutlet weak var multipleLabel3: UILabel!
+    @IBOutlet weak var multipleLabel4: UILabel!
+    
     @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedLabel3: UILabel!
+    @IBOutlet weak var rangedLabel4: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +48,46 @@ class QuestionViewController: UIViewController {
         navigationItem.title = "Question #\(questionIndex+1)"
         
         let currentQuestion = question[questionIndex]
+        let currentAnswer = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(question.count)
+        
+        questionLabel.text = currentQuestion.text
+        
+        questionProgressView.setProgress(totalProgress, animated: true)
         
         switch currentQuestion.type {
         case .single:
-            singleStackView.isHidden = false
+            updateSingleStack(using: currentAnswer)
         case .multiple:
-            multipleStackView.isHidden = false
+            updateMultipleStack(using: currentAnswer)
         case .ranged:
-            rangedStackView.isHidden = false
+            updateRangedStack(using: currentAnswer)
         }
+    }
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multipleLabel1.text = answers[0].text
+        multipleLabel2.text = answers[1].text
+        multipleLabel3.text = answers[2].text
+        multipleLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers[1].text
+        rangedLabel3.text = answers[2].text
+        rangedLabel4.text = answers.last?.text
+        
     }
     
     
